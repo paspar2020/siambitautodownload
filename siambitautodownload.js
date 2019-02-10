6679f63d26b8b6380f49133c74cf7d07d731a0c7
Shi_ba'San <thanatuth.euro@gmail.com>
2019-02-10 17:10:26 +0700

    Create siambitautodownload.js

---
+++ siambitautodownload.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        <$URL$>
+// @grant        none
+// ==/UserScript==
+var str = window.location.search;
+var strsub = window.location.search.substring(str.indexOf('?id')+4,str.indexOf('&'));
+if (strsub >= 7){sndReq('action=say_thank&id='+strsub, 'saythankup');
+
+var rawtext = document.documentElement.innerHTML.indexOf("downloadnew.php?id=");
+var rawtext2 = document.documentElement.innerHTML.indexOf("Download this file");
+var res = document.documentElement.innerHTML.substring(rawtext, rawtext2-7);
+window.location ="https://www.siambit.me/"+res;}
+
