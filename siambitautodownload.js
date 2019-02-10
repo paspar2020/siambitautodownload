0bbcefd0356f8b93f5669b117b59961217e9bbf3
Shi_ba'San <thanatuth.euro@gmail.com>
2019-02-10 17:31:09 +0700

    change status in title if it's download started

--- siambitautodownload.js
+++ siambitautodownload.js
@@ -14,5 +14,7 @@ if (strsub >= 7){sndReq('action=say_thank&id='+strsub, 'saythankup');
 var rawtext = document.documentElement.innerHTML.indexOf("downloadnew.php?id=");
 var rawtext2 = document.documentElement.innerHTML.indexOf("Download this file");
 var res = document.documentElement.innerHTML.substring(rawtext, rawtext2-7);
-window.location ="https://www.siambit.me/"+res;}
+window.location ="https://www.siambit.me/"+res;
+document.title = "Download started";
+                }
 
