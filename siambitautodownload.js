c4bb77b71bd9172423201c17e1806ae41f334ac9
Shi_ba'San <thanatuth.euro@gmail.com>
2019-03-13 00:55:48 +0700

    fix name and Site Working

--- siambitautodownload.js
+++ siambitautodownload.js
@@ -1,24 +1,27 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         SiambitAutoDownload
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        <$URL$>
+// @match        *.siambit.me
 // @grant        none
 // ==/UserScript==
 var str = window.location.search;
 var strsub = window.location.search.substring(str.indexOf('?id')+4,str.indexOf('&'));
-if (strsub >= 7){sndReq('action=say_thank&id='+strsub, 'saythankup');
-
+if (strsub >= 7){
+sndReq('action=say_thank&id='+strsub, 'saythankup');
 var rawtext = document.documentElement.innerHTML.indexOf("downloadnew.php?id=");
 var rawtext2 = document.documentElement.innerHTML.indexOf("Download this file");
 var res = document.documentElement.innerHTML.substring(rawtext, rawtext2-7);
 window.location ="https://www.siambit.me/"+res;
+
 document.title = "Download started";
                 }
+
 if (strsub >= 7){
 if(document.title != "Download started"){
 location.reload();
 }
 }
+
